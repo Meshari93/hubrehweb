4b1be84d3f08216572aa9d36e1e91e44a5cb865a
Meshari93 <almeshari93@gmail.com>
2018-01-26 14:04:25 +0300

    first commit

---
+++ app/RegisterCostomer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RegisterCostomer extends Model
+{
+  protected $table = 'registers';
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,303 @@
+<!doctype html>
+  <html lang="ar">
+<head>
+<title> HUBREH </title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/owl.theme.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+
+	<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Google Font -->
+<link href='https://fonts.googleapis.com/css?family=Poppins:400,500,600' rel='stylesheet' type='text/css'>
+
+
+
+</head>
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+<!-- =========================
+     PRE LOADER
+==============================--->
+<div class="preloader">
+
+	<div class="sk-rotating-plane"></div>
+
+</div>
+
+<!-- =========================
+     NAVIGATION LINKS
+============================== -->
+
+
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+	<div class="container">
+
+		<!-- navbar header -->
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand">HUBREH</a>
+		</div>
+
+		<div class="collapse navbar-collapse">
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#intro" class="smoothScroll"> تعرف علينا </a></li>
+				<li><a href="#overview" class="smoothScroll"> مزايا  </a></li>
+  				<li><a href="#register" class="smoothScroll">التسجيل </a></li>
+  				<li><a href="#contact" class="smoothScroll">تواصل معنا  </a></li>
+			</ul>
+
+		</div>
+	</div>
+</div>
+
+<!-- =========================
+    INTRO SECTION
+============================== -->
+<section id="intro" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<h3 class="wow bounceIn" data-wow-delay="0.9s">تطبيق هب ريح يرحب بكم</h3>
+				<h1 class="wow fadeInUp" data-wow-delay="1.6s">سجل الآن ليصلك تنبيه عند الانطلاق </h1>
+				<a href="#overview" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="2.3s">تعرف علينا اكثر</a>
+				<a href="#register" class="btn btn-lg btn-danger smoothScroll wow fadeInUp" data-wow-delay="2.3s">سجل الان</a>
+			</div>
+
+
+	 </div >
+	  <ul class="footer--links footer--app-links" style="float:right; list-style-type: none;">
+
+		<li>
+			<a href="#" target="_blank">
+				<img src="images/Apple.png" alt="apple" style="width:155px;height:50px; margin: 5px;">
+			</a>
+		</li>
+
+		<li>
+			<a href="#" target="_blank">
+				<img src="images/android.png" alt="android" style="width:155px;height:50px; margin: 5px;">
+			</a>
+		</li>
+
+	  </ul>
+
+	 </div>
+
+</section>
+
+
+<!-- =========================
+    OVERVIEW SECTION
+============================== -->
+<section id="overview" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-6 col-sm-6" data-wow-delay="0.6s">
+        <h3>
+            HUBREH
+        </h3>
+          <p>
+          HUBREH يقدم الكثير من الخيارات لمستخدميه ويسهل عليهم الحصول على الرفاهية.  ويفتخر بذلك حيث وبكل سهولة يستطيع المستخدمين عمل حجوزات لكثير من الخيارات التي تناسب جميع المناسبات. حيث الشاليهات الأنيقة لتستمتعو بجمال البحر والشعور بالاسترخاء والقصور المميزة بمساحتها الواسعة التي تجمع الاحبه في مكان واحد. والمزارع التي تأخذكم الى قلب الطبيعة الخلابة والاستمتاع بتجربة الحياة البسيطة.  و يصحبكم إلى رحلة برية لتستمتعو بجو المخيمات ونكتشف معاَ جمال الصحراء . و يمكنكم قضاء وقت ممتع مع الاهل والأحبة في الاستراحات المصممة لراحتهم التي تجعل من المتعه وقتاً لاينتهي وقاعات المناسبات  لإحياء افراحكم والتي تجعل من مناسباتكم ايام لاتُنسى. واهتمامنا بعرض الشقق وبيوت المدينة العصرية لتنعمون بسكن مريح في أي وقت. نحن في هب ريح لدينا القدرة على إسعاد الجميع . ونسعد ان نكون عند ثقتكم.
+
+        </p>
+			</div>
+
+			<div class="wow fadeInUp col-md-6 col-sm-6" data-wow-delay="0.9s">
+				<img src="images/overview-img.jpg" class="img-responsive" alt="Overview">
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    DETAIL SECTION
+============================== -->
+<section id="detail" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInLeft col-md-4 col-sm-4" data-wow-delay="0.3s">
+				<i class="fa fa-shield"></i>
+				<h3>حجز آمن</h3>
+				<p>نقوم بتسهيل عمليات الحجز عبر منصتنا الآمنة، ونعمل وفقاً لمعايير تضمن خصوصيتك. </p>
+			</div>
+
+			<div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="0.6s">
+				<i class="fa fa-clock-o"></i>
+				<h3> خدمة عملاء 24/7 </h3>
+				<p>يسعد فريق خدمة العملاء بالإجابة على أسئلتك على مدار الساعة، ويعملون لتلبية طلباتك .</p>
+			</div>
+
+			<div class="wow fadeInRight col-md-4 col-sm-4" data-wow-delay="0.9s">
+				<i class="fa fa-th-list"></i>
+				<h3>تشكيلة مذهلة من الخيارات</h3>
+				<p>HUBREH يقدم لك تشكيلة مذهلة واسعة من الخيارات في مكان واحد .</p>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    VIDEO SECTION
+============================== -->
+<section id="video" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.3s">
+				<h2>شاهد الفديو</h2>
+				<h3> شاهد الفيديو التعريفي لتعرف اكثر</h3>
+			</div>
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.6s">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XDPwXQjAlB0" allowfullscreen></iframe>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+
+<!-- =========================
+   REGISTER SECTION
+============================== -->
+<section id="register" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-7 col-sm-7" data-wow-delay="0.6s">
+				<h2>سجل هنا</h2>
+				<h3> كن اول من يحصل على التطبيق واستفد من جميع الخدمات الرائعة وسيصلك تنبيه عند إطلاق التطبيق.</h3>
+			</div>
+
+			<div class="wow fadeInUp col-md-5 col-sm-5" data-wow-delay="1s">
+				<form method="POST" action="{{ route('register') }}">
+          {{ csrf_field() }}
+
+					<input name="firstname" type="text" class="form-control" id="firstname" required placeholder="الاسم الاول">
+					<input name="lastname" type="text" class="form-control" id="lastname" required placeholder="الاسم الاخير">
+					<input name="phone" pattern="[0][5][0-9]{8}$" type="telephone" class="form-control" id="phone"  placeholder=" 0512345678 رقم التلفون">
+					<input name="email" type="email"  class="form-control" id="email" required placeholder="بريد الاكتروني">
+					<div class="col-md-offset-6 col-md-6 col-sm-offset-1 col-sm-10">
+						<input name="submit" type="submit" class="form-control" id="submit" value="سجل">
+					</div>
+				</form>
+			</div>
+
+			<div class="col-md-1"></div>
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    FAQ SECTION
+============================== -->
+
+
+<!-- =========================
+    CONTACT SECTION
+============================== -->
+<section id="contact" class="parallax-section" >
+	<div class="container" >
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-offset-1 col-md-5 col-sm-6" data-wow-delay="0.6s">
+				<div class="contact_des">
+					<h3>تواصل معنا</h3>
+					<p> ويسرنا أن تعلموا أننا في HUBREH موجودين لخدمتكم وتقديم أفضل خدمات الحجوزات لكم. وسوف يتم دراسة ملاحظاتكم ومقترحاتكم حول خدماتنا بكل جدية إذ وجدنا أنه من واجبنا التواصل معكم للارتقاء إلى المستوى اللائق بكم.</p>
+					<!-- <a href="#" class="btn btn-danger">DOWNLOAD NOW</a> -->
+				</div>
+			</div>
+
+			<div class="wow fadeInUp col-md-5 col-sm-6" data-wow-delay="0.9s">
+				<div class="contact_detail">
+					<div class="section-title">
+						<h2>اترك لنا اقترحاتك</h2>
+					</div>
+					<form method="POST" action="{{ route('note') }}">
+          			{{ csrf_field() }}
+						<input name="name" type="text" class="form-control" id="name" placeholder="الاسم">
+					  	<input name="email" type="email" class="form-control" id="email" placeholder="البريد الاكتروني">
+					  	<textarea name="message" rows="5" class="form-control" id="message" placeholder="نص الرسالة"></textarea>
+						<div class="col-md-6 col-sm-10">
+							<input name="submit" type="submit" class="form-control" id="submit" value="ارسال">
+						</div>
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    FOOTER SECTION
+============================== -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12"> 
+				<p class="wow fadeInUp" data-wow-delay="0.6s">Copyright &copy; 2018 HUBREH </p>
+
+				<ul class="social-icon">
+					<li><a href="https://www.facebook.com/hubreh.hubreh.5" class="fa fa-facebook wow fadeInUp" data-wow-delay="1s"></a></li>
+					<li><a href="https://twitter.com/HUBREH_app" class="fa fa-twitter wow fadeInUp" data-wow-delay="1.5s"></a></li>
+					<li><a href="https://www.instagram.com/" class="fa fa-instagram wow fadeInUp" data-wow-delay="2s"></a></li>
+					<!-- href="#" class="fa fa-dribbble wow fadeInUp" data-wow-delay="1.6s"></a></li> -->
+					<!--  <li><a href="#" class="fa fa-behance wow fadeInUp" data-wow-delay="1.9s"></a></li> -->
+					<li><a href="https://plus.google.com/u/0/104909016875304250496" class="fa fa-google-plus wow fadeInUp" data-wow-delay="2.5s"></a></li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+</footer>
+
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS
+==============================-->
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.parallax.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
